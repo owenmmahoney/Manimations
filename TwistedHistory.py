c86c52fd6d3dfe61bcd960f1bc44375c66b67985
owenmmahoney <owenmmahoney@proton.me>
2026-05-11 08:52:16 -0500

    Intro thingy

--- TwistedHistory.py
+++ TwistedHistory.py
@@ -5,47 +5,31 @@ class Intro(Scene):
     def construct(self):
         
         longino_color = m.ManimColor("#F8E0AD")
-        longino_py = m.ImageMobject(r"assets/longino-pyramid.png").scale(0.875)
-        longino_py_svg = m.SVGMobject(r"assets/longino-pyramid-svg.svg").scale(1.25)
-        longino_py_svg.set_color(longino_color)
+        longino_py = m.ImageMobject(r"assets/longino-pyramid.png")
+        longino_py_svg = m.SVGMobject(r"assets/longisvg.svg").scale(1.25)
+        longino_py.match_height(longino_py_svg)
+        longino_py.match_height(longino_py_svg)
         longino_py_svg.move_to(m.ORIGIN)
+        longino_py_svg.move_to(m.RIGHT * 3.0)
+        longino_py.move_to(m.ORIGIN)
+        longino_py.move_to(m.RIGHT * 3.0)
         text = m.Text("Longino Films", font_size=85, font="Times New Roman", color=longino_color)
         text.move_to(m.ORIGIN)
         peak = m.Text("so peak", font_size=35, font="Times New Roman", color=longino_color)
+        text.next_to(longino_py, m.LEFT, buff=0.75)
+        longino_py.set_z_index(30)
 
         self.wait(0.5)
 
         self.play(
             m.Write(longino_py_svg),
-            run_time=2.5
-        )
-
-        self.wait(0.5)
-
-        longino_py.move_to(longino_py_svg.get_center())
-
-        self.play(
-            m.FadeTransform(longino_py_svg, longino_py),
-            run_time=2
-        )
-
-
-        self.play(
-            longino_py.animate.move_to(m.RIGHT * 3.0),
-            run_time=1.5,
-            rate_func=m.smooth
-        )
-
-        text.next_to(longino_py, m.LEFT, buff=0.75)
-        
-        self.play(
             m.Write(text, lag_ratio=0.05),
-            run_time=2.575
+            run_time=3
         )
 
-        intro_group = m.Group(longino_py, text)
+        self.wait(1.5)
 
-        self.wait(1.25)
+        intro_group = m.Group(text, longino_py_svg)
         
         self.play(
             m.FadeOut(intro_group),
@@ -67,7 +51,7 @@ class Intro(Scene):
         lf = m.Text("Longino Films Presents:", font_size=60, font="Times New Roman", color=(longino_color))
         written_by = m.Text("A Film Written By:\nOwen Mahoney & Bennett Rogers", font_size=60, font="Times New Roman", color=(longino_color))
         featuring = m.Text("Featuring Special Guest Stars: ", font_size=60, font="Times New Roman", color=(longino_color))
-        slb = m.Text("Steven Lucas Bently", font_size=60, font="Times New Roman", color=(longino_color))
+        slb = m.Text("Steven Lucas Bentley", font_size=60, font="Times New Roman", color=(longino_color))
         agk = m.Text("as Ghenghis Khan", font_size=45, font="Times New Roman", color=(longino_color))
         a = m.Text("&", font_size=60, font="Times New Roman", color=(longino_color))
         fgen4runnerimb = m.Text("First Gen 4Runner in Medium Blue", font_size=60, font="Times New Roman", color=(longino_color))
@@ -135,5 +119,23 @@ class Intro(Scene):
 
         self.play(
             m.FadeOut(fg4r),
+            run_time=2
+        )
+
+class Test(Scene):
+    def construct(self):
+
+        longino_py = m.ImageMobject(r"assets/longino-pyramid.png")
+        longino_py_svg = m.SVGMobject(r"assets/longisvg.svg").scale(1.25)
+        longino_py.match_height(longino_py_svg)
+        longino_py.match_height(longino_py_svg)
+        
+        self.play(
+            m.DrawBorderThenFill(longino_py_svg),
+            run_time=3
+        )
+
+        self.play(
+            m.FadeTransform(longino_py_svg, longino_py),
             run_time=2
         )
